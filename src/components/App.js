4e36666c344eb9cad8954cc4de2f2bafc715d032
Rawand Dargali <rdargali@yahoo.com>
2020-08-31 00:33:25 -0500

    remove comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Body, Particles } from "./style.js";
-// import { Body, Footer, Particles } from "./style.js";
-// import { Body, Tl, Bl, Footer, Particles } from "./style.js";
 import particles from "../particles";
-
-// import tl from "./images/tl.png";
-// import br from "./images/br.png";
-
 import Navigation from "./Navigation";
 import Home from "./Home";
 import Skills from "./Skills";
